77c653d7c2b2fbe71e7d292e6532ef36f4a406b9
Yardanico <21169548+Yardanico@users.noreply.github.com>
2019-10-15 18:41:14 +0300

    Tianma display bringup, probably won't work, but at least it compiles

--- drivers/misc/mediatek/lcm/rules.mk
+++ drivers/misc/mediatek/lcm/rules.mk
@@ -1,17 +1,4 @@
 #
-# Copyright (C) 2015 MediaTek Inc.
-#
-# This program is free software: you can redistribute it and/or modify
-# it under the terms of the GNU General Public License version 2 as
-# published by the Free Software Foundation.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-# GNU General Public License for more details.
-#
-
-#
 # Makefile for misc devices that really don't fit anywhere else.
 #
 LOCAL_DIR := $(GET_LOCAL_DIR)
@@ -22,12 +9,6 @@ DEFINES += MTK_LCM_PHYSICAL_ROTATION=\"$(MTK_LCM_PHYSICAL_ROTATION)\"
 
 LCM_LISTS := $(subst ",,$(CONFIG_CUSTOM_LK_LCM))
 OBJS += $(foreach LCM,$(LCM_LISTS),$(LOCAL_DIR)/$(LCM)/$(addsuffix .o, $(LCM)))
-OBJS += $(LOCAL_DIR)/mt65xx_lcm_list.o \
-		$(LOCAL_DIR)/lcm_common.o \
-		$(LOCAL_DIR)/lcm_gpio.o \
-		$(LOCAL_DIR)/lcm_i2c.o \
-		$(LOCAL_DIR)/lcm_pmic.o \
-		$(LOCAL_DIR)/lcm_util.o
+OBJS += $(LOCAL_DIR)/mt65xx_lcm_list.o
 
-INCLUDES += -I$(LOCAL_DIR)/inc
 
